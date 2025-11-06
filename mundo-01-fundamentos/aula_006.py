25ea0185cbed656e6aa2d8b586ba0d200437e5e8
kdsousa <keplin.s.sousa@outlook.com>
2025-11-06 16:32:57 -0300

    aula(05 e 06) e desafio concluido

---
+++ mundo-01-fundamentos/aula_006.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print(type(n1))
+print(f'A soma é: {s}')
+
+n = input('Digite algo: ')
+print(n.isnumeric()) # se é numero
+print(n.isalpha()) # se é letra
+print(n.isalnum()) # se é alfanumerico
+print(n.isupper()) # ver se esta tudo em maiuscula
