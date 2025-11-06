69e8fc9da6cf8732e5fac3fad61e964d5fac1a26
kdsousa <keplin.s.sousa@outlook.com>
2025-11-06 00:13:29 -0300

    aula e desafio concluidos

---
+++ mundo-01-fundamentos/desafio_003.py
@@ -0,0 +1,4 @@
+""" Crie um script Python que leia dois numeros e tente mostrar a soma antes eles """
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo numero: '))
+print(f'A soma dos numeros {n1} e {n2} é de {n1+n2}')
