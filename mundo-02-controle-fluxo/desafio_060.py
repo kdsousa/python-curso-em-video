1f7a3d2d9f7dd5947f396784fca5c37e718f6f12
kdsousa <keplin.s.sousa@outlook.com>
2025-11-14 18:41:27 -0300

    aula e desafios concluidos

---
+++ mundo-02-controle-fluxo/desafio_060.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número qualquer e mostre o seu fatorial.
+# EX: 5 = 5x4x3x2x1 = 120
+from math import factorial
+n = int(input('Digite um número para calcular seu Fatorial: '))
+f = factorial(n)
+print(f'O fatorial de {n} é {f}')
---
+++ mundo-02-controle-fluxo/desafio_065.py
@@ -0,0 +1,19 @@
+# Crie um programa que leia vários números inteiros pelo teclado. No final 
+# da execução, mostre a média entre todos os valores e qual foi
+# o maior e o menor valores lidos. O programa deve perguntar ao usuários se ele quer ou não digitar valores.
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} e a média foi {média}')
+print(f'O maior valor foi {maior} e o menor {menor}')
